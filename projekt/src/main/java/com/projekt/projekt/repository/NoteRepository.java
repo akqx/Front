b78ff58efe2d18dca9d37e0f2df95da7ac2946be
akqx <kurelagnieszka@gmail.com>
2019-01-21 20:58:26 +0100

    Controllers

--- projekt/src/main/java/com/projekt/projekt/repository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projekt.projekt.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projekt.projekt.model.Note;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-
-}
