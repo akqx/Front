6d4fe7db640da123309bb499d018931644c0e8ec
akqx <kurelagnieszka@gmail.com>
2019-01-23 14:42:28 +0100

    change database 2

--- projekt/src/main/java/com/projekt/projekt/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projekt.projekt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.projekt.projekt.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
--- projekt/src/main/java/com/projekt/projekt/repository/UserroleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projekt.projekt.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.projekt.projekt.model.Userrole;
-
-public interface UserroleRepository extends JpaRepository<Userrole, Long> {
-
-}
